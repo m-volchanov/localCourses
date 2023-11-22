6e71d9307f625410d23467519bafbd4e2fcda296
m.volchanov <mishavolchanov@gmail.com>
2023-11-22 20:07:33 +0200

    adding code with lesson26

---
+++ src/test/java/lessons/lesson26/TestFrameExample.java
@@ -0,0 +1,26 @@
+package lessons.lesson26;
+
+import init.WebDriverInit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static java.lang.Thread.sleep;
+
+public class TestFrameExample extends WebDriverInit {
+
+    @Test
+    public void testFrame() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/iframe");
+
+        WebElement frame = driver.findElement(By.id("mce_0_ifr"));
+        driver.switchTo().frame(frame);
+
+        WebElement inputInFrame = driver.findElement(By.id("tinymce"));
+        sleep(5000);
+        inputInFrame.clear();
+        inputInFrame.sendKeys("Hello world. I am here");
+
+        sleep(5000);
+    }
+}
