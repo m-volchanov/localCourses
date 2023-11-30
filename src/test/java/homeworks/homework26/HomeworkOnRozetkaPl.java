e1caa680e8055fc8b1aa5b174448f6c2345f3d9b
m.volchanov <mishavolchanov@gmail.com>
2023-11-30 15:15:28 +0200

    adding code with homework26

--- src/test/java/homeworks/homework26/HomeworkOnRozetkaPl.java
+++ src/test/java/homeworks/homework26/HomeworkOnRozetkaPl.java
@@ -30,8 +30,7 @@ public class HomeworkOnRozetkaPl extends WebDriverInit {
     }
 
     private void handleCookiesNotification() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement cookiesNotification = wait.until(ExpectedConditions.presenceOfElementLocated(
+        WebElement cookiesNotification = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(
                 By.xpath("//button[@class='button button_size_large button_color_green']")));
         if (cookiesNotification.isDisplayed()) {
             cookiesNotification.click();
@@ -39,18 +38,16 @@ public class HomeworkOnRozetkaPl extends WebDriverInit {
     }
 
     private void goToLaptopsSection() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement laptopsAndTabletsCategory = wait.until(ExpectedConditions.visibilityOfElementLocated(
+        WebElement laptopsAndTabletsCategory = webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("(//a[@class='menu-categories__link'])[1]")));
         laptopsAndTabletsCategory.click();
-        WebElement laptopsCategory = wait.until(ExpectedConditions.elementToBeClickable(
+        WebElement laptopsCategory = webDriverWait.until(ExpectedConditions.elementToBeClickable(
                 By.xpath("(//a[@class='portal-navigation__link'])[1]")));
         laptopsCategory.click();
     }
 
     private void applyBrandFilter() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement brandFilter = wait.until(ExpectedConditions.elementToBeClickable(
+        WebElement brandFilter = webDriverWait.until(ExpectedConditions.elementToBeClickable(
                 By.xpath("(//a[contains(@class, 'checkbox-filter__link')])[5]")));
         brandFilter.click();
     }
@@ -67,8 +64,7 @@ public class HomeworkOnRozetkaPl extends WebDriverInit {
     }
 
     private void goToFirstProductWithActionLabel(String expectedActionLabel) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        List<WebElement> productsWithActionLabel = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(
+        List<WebElement> productsWithActionLabel = webDriverWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(
                 By.xpath("(//span[contains(@class, 'promo-label_type_action')])[1]")));
         Assert.assertFalse(productsWithActionLabel.isEmpty(), "Не найдены продукты с акционной плашкой.");
         WebElement firstProductWithActionLabel = productsWithActionLabel.get(0);
@@ -80,7 +76,7 @@ public class HomeworkOnRozetkaPl extends WebDriverInit {
         String actualActionLabel = actionLabel.getText();
         Assert.assertEquals(actualActionLabel, expectedActionLabel, "Метка акции не соответствует ожидаемой.");
         productName.click();
-        wait.until(ExpectedConditions.urlContains("p383969898"));
+        webDriverWait.until(ExpectedConditions.urlContains("p383969898"));
     }
 
     private void checkActionLabel() {
